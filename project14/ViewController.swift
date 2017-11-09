ded53ceeb53a7f83f6571c9650401f2a46b886a8
Sunny <sunnylee945@hotmail.com>
2017-11-09 16:40:07 +0800

    tableviewcell files

--- project14/ViewController.swift
+++ project14/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var tableView: UITableView!
+  enum DetailViewSection: Int{
+    case ProductPic = 0
+    case ProductDet = 1
+    case ProductSRF = 2
+    case Other = 3
+  }
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
